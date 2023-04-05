11bff48a466abdb09aa924c581e441ced8109138
Jia Chiuan <b1129046@cgu.edu.tw>
2023-04-06 00:35:58 +0800

    HW#3 one more step

--- HW#3/main.c
+++ HW#3/main.c
@@ -21,6 +21,13 @@ static int init(lotto* f){
     }
     return 0;
 }
+//對發票程式
+void lottery(int p){
+    int jackpot[3]={0};
+    printf("請輸入中獎三碼：");
+    scanf("%d %d %d",&jackpot[0],&jackpot[1],&jackpot[2]);
+    FILE* rc = fopen("record.bin","r+");
+}
 
 int main(){
 
@@ -34,15 +41,13 @@ int main(){
 
     //初始化
     init(&p);
-    
-    // printf("%d %d %d %d %d\n",p.num[0],p.num[1],p.num[2],p.num[3],p.num[4],p.num[5]);
 
 
     //接受輸入的數字
     int n;
     printf("welcome歡迎光臨長庚樂透購買機台\n""請問您要買幾組樂透彩：");
     scanf("%d[0 - 5]",&n);
-    if(n>5 || n<=0){
+    if(n>5 || n<0){
         printf("您的輸入有誤，請重新輸入");
         return 0;
     }
@@ -122,26 +127,14 @@ int main(){
         }
     }
 
-    
-    //開始輸出record.bin
-    FILE* rc = fopen("record.bin","ab+");
-    fprintf(rc, "%s"
-                "=======+ No.%05d +=====\n"
-                "%s"
-                "[1]:%02d %02d %02d %02d %02d %02d %02d\n"
-                "[2]:%02d %02d %02d %02d %02d %02d %02d\n"
-                "[3]:%02d %02d %02d %02d %02d %02d %02d\n"
-                "[4]:%02d %02d %02d %02d %02d %02d %02d\n"
-                "[5]:%02d %02d %02d %02d %02d %02d %02d\n",title,p.num,ctime(&curtime)
-                                                            ,p.a[0],p.a[1],p.a[2],p.a[3],p.a[4],p.a[5],p.a[6]
-                                                            ,p.b[0],p.b[1],p.b[2],p.b[3],p.b[4],p.b[5],p.b[6]
-                                                            ,p.c[0],p.c[1],p.c[2],p.c[3],p.c[4],p.c[5],p.c[6]
-                                                            ,p.d[0],p.d[1],p.d[2],p.d[3],p.d[4],p.d[5],p.d[6]
-                                                            ,p.e[0],p.e[1],p.e[2],p.e[3],p.e[4],p.e[5],p.e[6]);
-    fclose(rc);
-
     //命名檔案
     char name[20];
+    lotto note;
+    rc = fopen("record.bin","r+");
+    while(fread(&note, sizeof(lotto), 1, rc)){
+        p.num++;
+    }
+    fclose(rc);
     sprintf(name,"lotto%05d.txt",p.num);
 
     //開始輸出檔案
@@ -161,6 +154,24 @@ int main(){
                                                             ,p.d[0],p.d[1],p.d[2],p.d[3],p.d[4],p.d[5],p.d[6]
                                                             ,p.e[0],p.e[1],p.e[2],p.e[3],p.e[4],p.e[5],p.e[6]);
     fclose(fp);
+    
+    //開始輸出record.bin
+    rc = fopen("record.bin","ab+");
+    fprintf(rc, "%s"
+                "=======+ No.%05d +=====\n"
+                "%s"
+                "[1]:%02d %02d %02d %02d %02d %02d %02d\n"
+                "[2]:%02d %02d %02d %02d %02d %02d %02d\n"
+                "[3]:%02d %02d %02d %02d %02d %02d %02d\n"
+                "[4]:%02d %02d %02d %02d %02d %02d %02d\n"
+                "[5]:%02d %02d %02d %02d %02d %02d %02d\n",title,p.num,ctime(&curtime)
+                                                            ,p.a[0],p.a[1],p.a[2],p.a[3],p.a[4],p.a[5],p.a[6]
+                                                            ,p.b[0],p.b[1],p.b[2],p.b[3],p.b[4],p.b[5],p.b[6]
+                                                            ,p.c[0],p.c[1],p.c[2],p.c[3],p.c[4],p.c[5],p.c[6]
+                                                            ,p.d[0],p.d[1],p.d[2],p.d[3],p.d[4],p.d[5],p.d[6]
+                                                            ,p.e[0],p.e[1],p.e[2],p.e[3],p.e[4],p.e[5],p.e[6]);
+    fclose(rc);
+
     printf("已為您購買的 %d 組樂透書出至lotto.txt\n",n);
     return 0;
 }
