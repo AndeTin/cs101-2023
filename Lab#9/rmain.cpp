1e58998387123585392dbc87eb9ad92ffa60b10b
Jia Chiuan <b1129046@cgu.edu.tw>
2023-05-25 15:09:47 +0800

    Create rmain.cpp

---
+++ Lab#9/rmain.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class ReplaceMyString {
+    public:
+        string oldString;
+        string newString;
+        void replaceString(string x, string y) {
+            ifstream file;
+            ofstream newfile;
+            file.open("main.cpp");
+            newfile.open("rmain.cpp");
+            while(!file.eof()){
+                getline(file,oldString);
+                size_t found = oldString.find(x);
+                while(found!=string::npos){
+                    oldString.replace(found , x.length() , y);
+                    found = oldString.find(x, found + x.length());
+                }
+                newfile<<oldString<<endl;
+            }
+            file.close();
+            newfile.close();
+        }
+};
+
+int main(){
+    ReplaceMyString my;
+    my.replaceString("IU is best", "IU is best is best");
+    return 0;
+    }
